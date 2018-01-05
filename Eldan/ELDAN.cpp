88993aaf969756092e416ae70855ef26c8bb8726
Eldan <eldan.abdrashim@gmail.com>
2018-01-05 20:31:13 +0600

    asd

---
+++ Eldan/ELDAN.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#if __cplusplus > 199711L
+#include <unordered_set>
+#include <unordered_map>
+#include <tuple>
+#endif
+
+using namespace std;
+ 
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define sqr(x) ((x) * (x))
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair <ll, ll> pll;
+typedef pair <int, int> pii;
+ 
+const double eps = 1e-9;
+const double pi = acos(-1.0); 
+const int mod = (int)1e9 + 7;
+
+
+int main() { 
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif   
+    
+
+
+    return 0;
+}
