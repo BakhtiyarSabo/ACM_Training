5365720c4fa1b6096fad502dfb4b5f97114df253
Eldan <eldan.abdrashim@gmail.com>
2018-01-06 19:25:09 +0600

    fisads

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// "Brevity is the soul of wit."
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define boost ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(a) (a).begin(), (a).end()
+#define sz(a) (int)(a).size()
+#define pb push_back
+#define ull unsigned long long
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+
+int main() { boost
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+#endif  
+    
+    
+
+    return 0;
+}
