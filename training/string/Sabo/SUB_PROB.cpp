3f5837e6715c03f69bb9a9d9364f6dcaa2010a86
sabo <bakhtiyarkbtu@gmail.com>
2018-01-11 14:56:39 +0600

    sub orib

--- training/string/Sabo/SUB_PROB.cpp
+++ training/string/Sabo/SUB_PROB.cpp
@@ -10,16 +10,17 @@
 using namespace std;
 
 const int P = 131;
-#define ll long long
+#define ll unsigned long long
 ll h[1000010], p[1000010], H;
 
 
 int n, m, N;
 string s, t;
+char c[1000010];
 
 inline void calc_p(){
 	p[0] = 1;
-	for(int i = 1; i < 1000009; i++) p[i] = p[i-1] * P;
+	for(int i = 1; i <= m; i++) p[i] = p[i-1] * P;
 
 	if(s[0] >= 'A' && s[0] <= 'Z') h[0] = (s[0] - 'A' + 1);
 	else h[0] = (s[0] - 'a' + 27);
@@ -30,47 +31,50 @@ inline void calc_p(){
 	}
 }
 
-vector <pair <int, ll> > hash;
+vector <pair <int, ll> > HASSS;
 
 
 int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> s >> N;
+	// ios_base::sync_with_stdio(0);
+	// cin.tie(0); cout.tie(0);
+	scanf("%s", c);
+	scanf("%d", &N);
+	s = string(c);
 	m = s.size();
 	calc_p();
 
 	for(int i = 0; i < N; i++){
-		cin >> t;
+		scanf("%s", c);
+		t = string(c);
 		H = 0;
 		if(t[0] >= 'A' && t[0] <= 'Z') H = (t[0] - 'A' + 1);
 		else H = (t[0] - 'a' + 27);
 
 		for(int j = 1; j < t.size(); j++){
-			if(t[i] >= 'A' && t[i] <= 'Z') H += p[i] * (t[i] - 'A' + 1);
-			else H += p[i] * (t[i] - 'a' + 27);
+			if(t[j] >= 'A' && t[j] <= 'Z') H += p[j] * (t[j] - 'A' + 1);
+			else H += p[j] * (t[j] - 'a' + 27);
 		}
-		hash.push_back(make_pair(int(t.size()), H));
+		HASSS.push_back(make_pair(int(t.size()), H));
 	}
 
-	for(int i = 0; i < hash.size(); i++){
+	for(int i = 0; i < HASSS.size(); i++){
 		bool nf = true;
-		for(int j = 0; j < s.size() - hash[i].first + 1; j++){
-			if(j == 0){
-				if(h[j + hash[i].first - 1] == hash[i].second * p[j]){
-					nf = false;
-					cout << "Y\n";
-					break;
-				}
+		if(HASSS[i].first > m){
+			puts("N");
+			continue;
+		}
+		for(int j = 0; j < s.size() - HASSS[i].first + 1; j++){
+			ll cur = h[j + HASSS[i].first - 1];
+			if(j > 0){
+				cur -= h[j-1];
 			}
-			else{
-				if(h[j + hash[i].first - 1] - h[j-1] == hash[i].second * p[j]){
-					nf = false;
-					cout << "Y\n";
-					break;
-				}
+
+			if(cur == HASSS[i].second * p[j]){
+				puts("Y");
+				nf = false;
+				break;
 			}
 		}
-		if(nf) cout << "N\n";
+		if(nf) puts("N");
 	}
 }
